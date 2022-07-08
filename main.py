8855c536f1f32774c1baf52be16eeae767c53ae7
GAldereteHero <herogonzalo@gmail.com>
2022-07-08 19:21:25 -0300

    Proyecto corriendo con bulma y jinja2

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse, FileResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory=".\public\static"), name="static")
+
+templates = Jinja2Templates(directory="./templates")
+
 @app.get('/', response_class=HTMLResponse)
 def index():
-    htmlAdress = ".\public\pages\index.html"
+    htmlAdress = ".\public\static\html\index.html"
     return FileResponse(htmlAdress, status_code=200)
+
+@app.get('/dashboard', response_class=HTMLResponse)
+def index(request:Request):
+    return templates.TemplateResponse("list.html",{"request":request,"name":'Adrian'})
---
+++ public/static/js/index.js
@@ -0,0 +1,1 @@
+alert("Holiisss buenas, Adriiian!! soy JavaScrit");
